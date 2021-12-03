46c7ab49c6a0db4b40eefeb7eee8bae6d7a6a4b6
Multach <matt.multach87@gmail.com>
2021-12-02 16:37:56 -0800

    -Upload Regex scratch paper

---
+++ Scratch_Paper/Scratch_RegEx.R
@@ -0,0 +1,29 @@
+# <span style = "color:green">`` `r "([^!.?\s]+)_([^!.?\s]+)"` ``</span>
+
+# ([^!.?\s]+)_([^!.?\s]+)
+
+* code_chunk_formatting
+([^!.?\s]+)_([^!.?\s]+)
+([A-Z a-z 0-9]+)_([A-Z a-z 0-9]+)
+([A-Z a-z 0-9 \]+)
+
+
+@aZouHastie2005
+\textcite{aaaaa9ZouHastie2005}
+\textcite{a9ZouHastie2005asdfasdfassdflkjsdf}
+
+\\textcite{([A-Z a-z 0-9]+)}{([0-9]+)}
+@$1HELLOW$2
+\\textcite{([A-Z a-z 0-9]+)}{([0-9]+)} --> @
+
+\\textcite{([A-Z a-z 0-9]+)}{([0-9]+)}
+
+
+# _([A-Z a-z 0-9 _ .]+)([(][)])_
+
+
+# **<span style = "color:blue">`` `r "$1$2"` ``</span>**
+
+_map()_ the _coefs_freq()_ _data.frame()_
+_map()_
+_([A-Z a-z 0-9 _ .]+)\(\)_
